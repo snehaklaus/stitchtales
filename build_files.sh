457c3ddfb6e6d68aea50a827d2c66b01ea62d7fa
sneha <sneha.naik1117@gmail.com>
2026-03-24 11:55:00 +0530

    Fix build issues

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Install dependencies
-pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 
-# Collect static files
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
