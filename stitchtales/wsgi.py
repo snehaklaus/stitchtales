c7c7b9b8ef56cde65af963068f007ea396f9433f
sneha <sneha.naik1117@gmail.com>
2026-03-24 12:10:35 +0530

    Vercel final fixes

--- stitchtales/wsgi.py
+++ stitchtales/wsgi.py
@@ -4,6 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stitchtales.settings')
 
 application = get_wsgi_application()
-
-# Vercel serverless handler
-app = application
