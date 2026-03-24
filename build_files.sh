260365daf9c71d65dc6cdb47c859641ba0951407
sneha <sneha.naik1117@gmail.com>
2026-03-24 14:05:17 +0530

    Direct trial on vercel

--- build_files.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip install -r requirements.txt --break-system-packages
-python manage.py collectstatic --noinput
--- stitchtales/wsgi.py
+++ stitchtales/wsgi.py
@@ -1,8 +1,16 @@
+"""
+WSGI config for stitchtales project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stitchtales.settings')
 
 application = get_wsgi_application()
-app = application  # ← Vercel looks for this
-
